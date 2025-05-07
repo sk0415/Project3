70f002f333a7d2024c8dab3cde804112526224e2
unknown <sanjana1kotha@gmail.com>
2025-05-07 17:57:04 -0500

    iterative insert

--- project3.py
+++ project3.py
@@ -5,10 +5,9 @@ BLOCK_SIZE = 512
 EMPTY_ROOT_ID = 0
 INITIAL_NEXT_BLOCK_ID = 1 
 
-MAX_KEYS = 3
-MAX_CHILDREN = 4
+MAX_KEYS = 19
+MAX_CHILDREN = 20
 
-# Helper: Create a new node block
 def create_node_block(block_id, parent_id, keys, values, children, num_keys=None):
     block = bytearray(BLOCK_SIZE)
     block[0:8] = block_id.to_bytes(8, 'big')
@@ -72,20 +71,6 @@ def parse_node_block(block):
         'children': children
     }
 
-def create_file(filename):
-    if os.path.exists(filename):
-        print("ERROR : Index file already exists.")
-        return
-
-    header_block = bytearray(BLOCK_SIZE)
-    header_block[0:8] = b'4348PRJ3'
-    header_block[8:16] = EMPTY_ROOT_ID.to_bytes(8, 'big')
-    header_block[16:24] = INITIAL_NEXT_BLOCK_ID.to_bytes(8, 'big')
-
-    with open(filename, 'wb') as f:
-        f.write(header_block)
-    print(f"Created index file: {filename}")
-
 def update_header(f, root_id, next_block_id):
     f.seek(8)
     f.write(root_id.to_bytes(8, 'big'))
@@ -94,9 +79,6 @@ def update_header(f, root_id, next_block_id):
 def key_exists(f, root_id, search_key):
     if root_id == 0:
         return False
-        
-    # At most 3 nodes in memory at a time
-    # We'll use an iterative approach for the search
     curr_id = root_id
     while curr_id != 0:
         block = read_block(f, curr_id)
@@ -118,207 +100,164 @@ def insert(filename, key, value):
         return
 
     with open(filename, 'r+b') as f:
-        # Read header
         f.seek(0)
         magic = f.read(8)
         if magic != b'4348PRJ3':
             print("ERROR: Not a valid index file.")
             return
 
-        # Read root id and next block id
         root_id = int.from_bytes(f.read(8), 'big')
         next_block_id = int.from_bytes(f.read(8), 'big')
 
-        # Check if key already exists
-        if root_id != 0 and key_exists(f, root_id, key):
-            print(f"Key {key} already exists. Insertion aborted.")
-            return
-
-        # Allocate a new block ID
         def allocate_block():
             nonlocal next_block_id
             block_id = next_block_id
             next_block_id += 1
             return block_id
 
-        # Split a node and return the promoted key, value, and child IDs
-        def split_node(node, new_key=None, new_value=None, new_child_left=None, new_child_right=None):
-            # Create arrays with our new key/value to split
-            keys = node['keys'].copy()
-            values = node['values'].copy()
-            children = node['children'].copy()
-            
-            # If we have a new key to insert, add it first
-            if new_key is not None:
-                i = 0
-                while i < len(keys) and new_key > keys[i]:
-                    i += 1
-                    
-                keys.insert(i, new_key)
-                values.insert(i, new_value)
-                
-                # Update children if this is an internal node
-                if new_child_left is not None:
-                    children[i] = new_child_left
-                    children.insert(i+1, new_child_right)
-            
-            # Find middle index for split
-            mid = len(keys) // 2
-            
-            # Create left node
-            left_id = allocate_block()
-            left_keys = keys[:mid]
-            left_values = values[:mid]
-            left_children = children[:mid+1]
-            
-            # Create right node
+        def update_header_local(root_id):
+            nonlocal next_block_id
+            f.seek(8)
+            f.write(root_id.to_bytes(8, 'big'))
+            f.write(next_block_id.to_bytes(8, 'big'))
+
+        if root_id != 0 and key_exists(f, root_id, key):
+            print(f"Key {key} already exists.")
+            return
+
+        if root_id == 0:
+            root_id = allocate_block()
+            block = create_node_block(root_id, 0, [key], [value], [0] * MAX_CHILDREN, 1)
+            write_block(f, root_id, block)
+            update_header_local(root_id)
+            # print(f"Inserted key {key} as root.")
+            return
+
+        path = []
+        curr_id = root_id
+
+        while curr_id != 0:
+            path.append(curr_id)
+            block = read_block(f, curr_id)
+            node = parse_node_block(block)
+            i = 0
+            while i < node['num_keys'] and key > node['keys'][i]:
+                i += 1
+            if node['children'][0] == 0:
+                break
+            curr_id = node['children'][i]
+
+        curr_id = path[-1]
+        block = read_block(f, curr_id)
+        node = parse_node_block(block)
+
+        i = 0
+        while i < node['num_keys'] and key > node['keys'][i]:
+            i += 1
+        node['keys'].insert(i, key)
+        node['values'].insert(i, value)
+        node['num_keys'] += 1
+
+        while True:
+            if node['num_keys'] <= MAX_KEYS:
+                updated_block = create_node_block(
+                    node['block_id'], node['parent_id'],
+                    node['keys'][:node['num_keys']],
+                    node['values'][:node['num_keys']],
+                    node['children'],
+                    node['num_keys']
+                )
+                write_block(f, node['block_id'], updated_block)
+                update_header_local(root_id)
+                # print(f"Inserted key {key}.")
+                return
+
+            mid = node['num_keys'] // 2
+            promoted_key = node['keys'][mid]
+            promoted_value = node['values'][mid]
+
+            left_keys = node['keys'][:mid]
+            left_values = node['values'][:mid]
+            left_children = node['children'][:mid + 1]
+            right_keys = node['keys'][mid + 1:]
+            right_values = node['values'][mid + 1:]
+            right_children = node['children'][mid + 1:] + [0] * (MAX_CHILDREN - len(node['children'][mid + 1:]))
+
+            left_id = node['block_id']
             right_id = allocate_block()
-            right_keys = keys[mid+1:]
-            right_values = values[mid+1:]
-            right_children = children[mid+1:]
-            
-            # Create and write the blocks, setting correct parent IDs
-            # (parent will be updated later)
-            left_block = create_node_block(left_id, node['parent_id'], left_keys, left_values, left_children, len(left_keys))
-            write_block(f, left_id, left_block)
-            
-            right_block = create_node_block(right_id, node['parent_id'], right_keys, right_values, right_children, len(right_keys))
-            write_block(f, right_id, right_block)
-            
-            # Update children's parent pointers
-            for i, child_id in enumerate(left_children):
+
+            if len(path) <= 1:
+                parent_id = 0
+                new_root_id = allocate_block()
+                new_children = [left_id, right_id] + [0] * (MAX_CHILDREN - 2)
+                new_root = create_node_block(
+                    new_root_id, 0,
+                    [promoted_key], [promoted_value],
+                    new_children,
+                    1
+                )
+                write_block(f, new_root_id, new_root)
+                parent_id = new_root_id
+                root_id = new_root_id
+            else:
+                parent_id = path[-2]
+
+            write_block(f, left_id, create_node_block(
+                left_id, parent_id, left_keys, left_values, left_children, len(left_keys)))
+            write_block(f, right_id, create_node_block(
+                right_id, parent_id, right_keys, right_values, right_children, len(right_keys)))
+
+            for child_id in left_children:
                 if child_id != 0:
                     child_block = read_block(f, child_id)
                     child_node = parse_node_block(child_block)
                     child_node['parent_id'] = left_id
-                    updated_block = create_node_block(
-                        child_id, left_id, 
-                        child_node['keys'], child_node['values'], 
-                        child_node['children'], child_node['num_keys']
+                    updated_child = create_node_block(
+                        child_id, left_id,
+                        child_node['keys'][:child_node['num_keys']],
+                        child_node['values'][:child_node['num_keys']],
+                        child_node['children'],
+                        child_node['num_keys']
                     )
-                    write_block(f, child_id, updated_block)
-            
-            for i, child_id in enumerate(right_children):
+                    write_block(f, child_id, updated_child)
+
+            for child_id in right_children:
                 if child_id != 0:
                     child_block = read_block(f, child_id)
                     child_node = parse_node_block(child_block)
                     child_node['parent_id'] = right_id
-                    updated_block = create_node_block(
-                        child_id, right_id, 
-                        child_node['keys'], child_node['values'], 
-                        child_node['children'], child_node['num_keys']
+                    updated_child = create_node_block(
+                        child_id, right_id,
+                        child_node['keys'][:child_node['num_keys']],
+                        child_node['values'][:child_node['num_keys']],
+                        child_node['children'],
+                        child_node['num_keys']
                     )
-                    write_block(f, child_id, updated_block)
-            
-            # Return the promoted key, value, and the two new node IDs
-            return keys[mid], values[mid], left_id, right_id
+                    write_block(f, child_id, updated_child)
 
-        def insert_recursive(node_id, key, value):
-            # Read the node
-            block = read_block(f, node_id)
+            if len(path) <= 1:
+                update_header_local(root_id)
+                # print(f"Root was split. New root created with key {promoted_key}.")
+                return
+
+            # print(f"Node {node['block_id']} split. Promoting key {promoted_key} to parent {parent_id}.")
+
+            path.pop()
+            parent_id = path[-1]
+            block = read_block(f, parent_id)
             node = parse_node_block(block)
-            
-            # Find position to insert
-            i = 0
-            while i < node['num_keys'] and key > node['keys'][i]:
-                i += 1
-                
-            # If this is a leaf node
-            if node['children'][0] == 0:
-                # If there's room, insert the key/value
-                if node['num_keys'] < MAX_KEYS:
-                    node['keys'].insert(i, key)
-                    node['values'].insert(i, value)
-                    node['num_keys'] += 1
-                    
-                    # Write the updated node
-                    updated_block = create_node_block(
-                        node['block_id'], node['parent_id'], 
-                        node['keys'], node['values'], 
-                        node['children'], node['num_keys']
-                    )
-                    write_block(f, node['block_id'], updated_block)
-                    return None
-                else:
-                    # Node is full, split it
-                    return split_node(node, key, value)
-            else:
-                # This is an internal node, recurse to the appropriate child
-                child_id = node['children'][i]
-                result = insert_recursive(child_id, key, value)
-                
-                # If no split occurred, we're done
-                if result is None:
-                    return None
-                    
-                # Otherwise, handle the split
-                promoted_key, promoted_value, left_id, right_id = result
-                
-                # If there's room in this node, insert the promoted key
-                if node['num_keys'] < MAX_KEYS:
-                    node['keys'].insert(i, promoted_key)
-                    node['values'].insert(i, promoted_value)
-                    node['children'][i] = left_id
-                    node['children'].insert(i+1, right_id)
-                    node['num_keys'] += 1
-                    
-                    # Write the updated node
-                    updated_block = create_node_block(
-                        node['block_id'], node['parent_id'], 
-                        node['keys'], node['values'], 
-                        node['children'], node['num_keys']
-                    )
-                    write_block(f, node['block_id'], updated_block)
-                    return None
-                else:
-                    # This node is full too, split it
-                    return split_node(node, promoted_key, promoted_value, left_id, right_id)
 
-        # Handle empty tree case
-        if root_id == 0:
-            # Create the root node
-            root_id = allocate_block()
-            root_block = create_node_block(root_id, 0, [key], [value], [0] * MAX_CHILDREN, 1)
-            write_block(f, root_id, root_block)
-            
-            # Update the header
-            update_header(f, root_id, next_block_id)
-            print(f"Inserted key {key} as root.")
-            return
-        
-        # Insert into the tree
-        result = insert_recursive(root_id, key, value)
-        
-        # If a split occurred at the root, create a new root
-        if result is not None:
-            promoted_key, promoted_value, left_id, right_id = result
-            
-            # Create a new root
-            new_root_id = allocate_block()
-            children = [left_id, right_id] + [0] * (MAX_CHILDREN - 2)
-            root_block = create_node_block(new_root_id, 0, [promoted_key], [promoted_value], children, 1)
-            write_block(f, new_root_id, root_block)
-            
-            # Update the parent pointers of the children
-            for node_id in [left_id, right_id]:
-                if node_id != 0:
-                    child_block = read_block(f, node_id)
-                    child_node = parse_node_block(child_block)
-                    child_node['parent_id'] = new_root_id
-                    updated_block = create_node_block(
-                        node_id, new_root_id, 
-                        child_node['keys'], child_node['values'], 
-                        child_node['children'], child_node['num_keys']
-                    )
-                    write_block(f, node_id, updated_block)
-            
-            # Update the header
-            update_header(f, new_root_id, next_block_id)
-            print(f"Root was split. New root created with key {promoted_key}.")
-        else:
-            # Update the header (next_block_id might have changed)
-            update_header(f, root_id, next_block_id)
-            print(f"Inserted key {key}.")
+            insert_index = 0
+            while insert_index < node['num_keys'] and promoted_key > node['keys'][insert_index]:
+                insert_index += 1
+
+            node['keys'].insert(insert_index, promoted_key)
+            node['values'].insert(insert_index, promoted_value)
+            node['children'][insert_index] = left_id
+            node['children'].insert(insert_index + 1, right_id)
+            if len(node['children']) > MAX_CHILDREN:
+                node['children'] = node['children'][:MAX_CHILDREN]
+            node['num_keys'] += 1
 
 def create_file(filename):
     if os.path.exists(filename):
@@ -332,22 +271,6 @@ def create_file(filename):
 
     with open(filename, 'wb') as f:
         f.write(header_block)
-
-def key_exists(f, root_id, search_key):
-    curr_id = root_id
-    while curr_id != 0:
-        block = read_block(f, curr_id)
-        node = parse_node_block(block)
-
-        for i in range(node['num_keys']):
-            if search_key == node['keys'][i]:
-                return True
-            elif search_key < node['keys'][i]:
-                curr_id = node['children'][i]
-                break
-        else:
-            curr_id = node['children'][node['num_keys']]
-    return False
 
 def search(filename, search_key):
     with open(filename, 'rb') as f:
@@ -479,6 +402,5 @@ def main():
         extract(sys.argv[2], sys.argv[3])
     else:
         print("Unknown command")
-
 if __name__ == "__main__":
     main()
